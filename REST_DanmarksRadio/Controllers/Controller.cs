17ccc51ecf1b0b7c52c9936b90d2af54096676e2
Patrick-LD <patricklykkedahl@gmail.com>
2026-04-13 12:17:38 +0200

    Har tilføjet getAll metoden til vores interface og updateret vores controller til at vise metoden i swagger Tilføjet swagger og CORS i program.cs

--- REST_DanmarksRadio/Controllers/Controller.cs
+++ REST_DanmarksRadio/Controllers/Controller.cs
@@ -1,83 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ClassLibary;
 
 namespace REST_DanmarksRadio.Controllers
 {
-    public class Controller : Microsoft.AspNetCore.Mvc.Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DRController : ControllerBase
     {
-        // GET: HomeController
-        public ActionResult Index()
+        private IRepository _repository;
+
+        public DRController(IRepository repository)
         {
-            return View();
+            _repository = repository;
         }
 
-        // GET: HomeController/Details/5
-        public ActionResult Details(int id)
+        // GET: api/DR
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult GetAll()
         {
-            return View();
-        }
-
-        // GET: HomeController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: HomeController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Ok(_repository.GetAll());
         }
     }
 }
--- REST_DanmarksRadio/Program.cs
+++ REST_DanmarksRadio/Program.cs
@@ -15,16 +15,36 @@ namespace REST_DanmarksRadio
             builder.Services.AddOpenApi();
             builder.Services.AddScoped<IRepository, Repository>();
 
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
