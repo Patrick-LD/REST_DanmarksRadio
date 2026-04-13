7ce43533caae169cdf11e0cd9cae486f02394734
Patrick-LD <patricklykkedahl@gmail.com>
2026-04-13 10:09:25 +0200

    Add project files.

---
+++ ClassLibary/Models/DR.cs
@@ -0,0 +1,11 @@
+﻿namespace REST_DanmarksRadio.Models
+{
+    public class DR
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public int Duration { get; set; }
+        public int publishedYear { get; set; }
+    }
+}
---
+++ ClassLibary/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibary
+{
+    public class Repository
+    {
+        private int nextId = 0;
+        private readonly List<REST_DanmarksRadio.Models.DR> _DRs = new List<REST_DanmarksRadio.Models.DR>();
+
+
+        public List<REST_DanmarksRadio.Models.DR> GetAll()
+        {
+            return new List<REST_DanmarksRadio.Models.DR>(_DRs);
+        }
+
+    }
+}
