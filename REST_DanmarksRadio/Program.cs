603db8d0c594ff0b23e65b70ac7e0588820b0286
Patrick-LD <patricklykkedahl@gmail.com>
2026-04-17 10:26:44 +0200

    lavet AUTH controller og log in

--- REST_DanmarksRadio/Program.cs
+++ REST_DanmarksRadio/Program.cs
@@ -1,6 +1,10 @@
 using ClassLibary;
 using ClassLibary.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace REST_DanmarksRadio
 {
@@ -9,6 +13,10 @@ namespace REST_DanmarksRadio
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // JWT settings
+            var jwtSettings = builder.Configuration.GetSection("Jwt");
+            var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]!);
 
             // Add services to the container.
 
@@ -19,9 +27,37 @@ namespace REST_DanmarksRadio
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IRepository, DB_Repo>();
 
-            // Swagger
+            // Swagger med JWT-understøttelse
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "DanmarksRadio API", Version = "v1" });
+
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token as: Bearer {your_token}",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
 
             // CORS
             builder.Services.AddCors(options =>
@@ -33,6 +69,28 @@ namespace REST_DanmarksRadio
                           .AllowAnyMethod();
                 });
             });
+
+            // JWT Authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            builder.Services.AddAuthorization();
 
 
             var app = builder.Build();
@@ -50,6 +108,7 @@ namespace REST_DanmarksRadio
 
             app.UseCors("AllowAll");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllers();
